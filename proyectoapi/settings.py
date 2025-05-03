7d9e940471f54b4ee41da0b15f6c5c243f54bb10
INGJuanOlivera <ingjuanoliveramoreno@gmail.com>
2025-05-02 20:34:55 -0500

    feat: database

--- proyectoapi/settings.py
+++ proyectoapi/settings.py
@@ -76,8 +76,15 @@ WSGI_APPLICATION = 'proyectoapi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'project_database',
+        'USER': 'root',
+        'PASSWORD': '0000',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
