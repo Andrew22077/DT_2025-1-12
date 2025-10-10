ffd183a526428a2e8c444e71c8fc6950efef9ccf
Andrew22077 <106720352+Andrew22077@users.noreply.github.com>
2025-10-09 22:35:40 -0500

    feat: test

--- backend/project_grado_api/settings.py
+++ backend/project_grado_api/settings.py
@@ -47,6 +47,22 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],  # Puedes crear esta carpeta si quieres usar templates personalizados
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
 # ======================
 #  URLS & WSGI
 # ======================
